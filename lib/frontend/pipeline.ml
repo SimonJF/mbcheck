fb3f719bb938e7986ec841de570c8e5c1773d2e8
Simon Fowler <simon@simonjf.com>
2023-05-22 10:08:36 +0100

    Add let: T sugar
    As used in the paper, might as well ensure same can be used here.
    Essentially this does the transformation
    
      let x : T = M in N
    
    -->
    
      let x = (M : returnable(T)) in N

--- lib/frontend/pipeline.ml
+++ lib/frontend/pipeline.ml
@@ -1,17 +1,21 @@
 open Common
 
+
+let desugar p =
+    p
+    |> Desugar_sugared_guards.desugar
+    |> Desugar_let_annotations.desugar
+    |> Insert_pattern_variables.annotate
+
+
 (* Frontend pipeline *)
 let pipeline p =
-    let p = Desugar_sugared_guards.desugar p in
-    let p = Insert_pattern_variables.annotate p in
-    (* Format.printf "PROGRAM: %a\n" Common.Sugar_ast.pp_program p; *)
+    let p = desugar p in
     let ir = Sugar_to_ir.transform p in
-    (*
     let () = Format.printf
         "=== Intermediate Representation: ===\n%a\n\n"
         (Common.Ir.pp_program) ir
     in
-    *)
     let ir, prety_opt = Typecheck.Pretypecheck.check ir in
     let (ty, env, constrs) = Typecheck.Gen_constraints.synthesise_program ir in
     let solution = Typecheck.Solve_constraints.solve_constraints constrs in
---
+++ test/pat-tests/let-annot-2.pat
@@ -0,0 +1,6 @@
+def main(): Int {
+    let x: Int = 5 + 5 in
+    x
+}
+
+main()
